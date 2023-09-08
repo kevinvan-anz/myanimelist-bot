3cc46216a8d1ef63708539fb01f20a24027f35b8
Van Nguyen <van.nguyen2@anz.com>
2023-09-08 11:52:50 +1000

    animedata processing

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 )
 
 func main() {
+	// Requesting MAL API
 	clientIDUsername := os.Getenv("MALCLIENTID")
 	publicInfoClient := &http.Client{
 		// Create client ID from https://myanimelist.net/apiconfig.
@@ -21,7 +22,8 @@ func main() {
 
 	ctx := context.Background()
 
-	anime, _, err := c.Anime.Details(ctx, 51009,
+	// Retrieving anime data fields
+	anime, _, err := c.Anime.Details(ctx, 51179,
 		mal.Fields{
 			"mean",
 			"rank",
@@ -45,19 +47,21 @@ func main() {
 		return
 	}
 
+	animeData := processAnimeData(*anime)
+
 	// Sample output for anime details
-	fmt.Printf("%s\n", anime.Title)
-	fmt.Printf("Score: %v\n", anime.Mean)
-	fmt.Printf("Ranking: #%d\n", anime.Rank)
-	fmt.Printf("Popularity: #%d\n", anime.Popularity)
-	fmt.Printf("Premier: %d %s\n", anime.StartSeason.Year, anime.StartSeason.Season)
+	fmt.Printf("%s\n", animeData.Title)
+	fmt.Printf("Score: %v\n", animeData.Mean)
+	fmt.Printf("Ranking: #%d\n", animeData.Rank)
+	fmt.Printf("Popularity: #%d\n", animeData.Popularity)
+	fmt.Printf("Premier: %s\n", animeData.StartSeason)
 	// TO DO: Convert JST value to AEST
-	fmt.Printf("Broadcast: %v at %v JST+1\n", anime.Broadcast.DayOfTheWeek, anime.Broadcast.StartTime)
-	for _, s := range anime.Studios {
-		fmt.Printf("Studio: %s\n", s.Name)
+	fmt.Printf("Broadcast: %s\n", animeData.Broadcast)
+	for _, studio := range animeData.Studios {
+		fmt.Printf("Studio: %s\n", studio)
 	}
 	fmt.Printf("Episodes: %d\n", anime.NumEpisodes)
-	fmt.Printf("Episode Duration: %d minutes\n", anime.AverageEpisodeDuration/60)
+	fmt.Printf("Episode Duration: %d minutes\n", animeData.AverageEpisodeMinutes)
 
 	fmt.Println("JST Time:", jstTime)
 	fmt.Println("AEST Time:", aestTime)
@@ -68,12 +72,50 @@ type clientIDTransport struct {
 	ClientID  string
 }
 
+// RoundTrip - Authentication for API client ID
 func (c *clientIDTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	if c.Transport == nil {
 		c.Transport = http.DefaultTransport
 	}
 	req.Header.Add("X-MAL-CLIENT-ID", c.ClientID)
 	return c.Transport.RoundTrip(req)
+}
+
+// AnimeData list
+type AnimeData struct {
+	Title                 string
+	Synopsis              string
+	Mean                  float64
+	Rank                  int
+	Popularity            int
+	StartSeason           string
+	Broadcast             string
+	Studios               []string
+	NumEpisodes           int
+	AverageEpisodeMinutes int
+}
+
+// processAnimeData - Processing data from AnimeData struct
+func processAnimeData(anime mal.Anime) AnimeData {
+	animeData := AnimeData{
+		Title:       anime.Title,
+		Synopsis:    anime.Synopsis,
+		Mean:        anime.Mean,
+		Rank:        anime.Rank,
+		Popularity:  anime.Popularity,
+		StartSeason: fmt.Sprintf("Premier: %d %s", anime.StartSeason.Year, anime.StartSeason.Season),
+		Broadcast:   fmt.Sprintf("Broadcast: %v at %v JST+1", anime.Broadcast.DayOfTheWeek, anime.Broadcast.StartTime),
+		// Need explanation
+		Studios:               make([]string, len(anime.Studios)),
+		NumEpisodes:           anime.NumEpisodes,
+		AverageEpisodeMinutes: anime.AverageEpisodeDuration / 60,
+	}
+
+	for i, s := range anime.Studios {
+		animeData.Studios[i] = s.Name
+	}
+
+	return animeData
 }
 
 func convertJSTToAEST(jstTimeStr string) (time.Time, time.Time, error) {
